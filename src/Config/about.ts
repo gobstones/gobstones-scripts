6fc20bb9a71e9b18ddf9033a298e6abdeecbacbc
Alan Rodas Bonjour <alanrodas@gmail.com>
2024-09-05 15:56:31 -0300

    build: bump the gobstones-core and typedoc-theme-gobstones versions

--- src/Config/about.ts
+++ src/Config/about.ts
@@ -23,7 +23,7 @@
  * is not reliable between different implementations of package managers
  * and that may lead to false readings.
  */
-export const version = '0.9.2';
+export const version = '0.9.3';
 
 /**
  * The tool's test server address.
